01a4d6b1b7e75814781289b043997f5c2d37bb81
lipan <lipan@cloudwalk.cn>
2019-08-28 11:35:22 +0800

    x

--- src/test/Fun1.java
+++ src/test/Fun1.java
@@ -4,11 +4,6 @@ public class Fun1 {
 
 	public static void main(String[] args) {
 		System.out.println("fun1");
-		System.out.println("fun1");
 	}
 	
-	public String sss(){
-		System.out.println("xxxx");
-		return "111";
-	}
 }
