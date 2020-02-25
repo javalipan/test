94615b57fe9a3aba85cfac03f5be1a885e423945
javalipan <43563693+javalipan@users.noreply.github.com>
2020-02-25 15:07:18 +0800

    1
    1

--- src/test/Fun1.java
+++ src/test/Fun1.java
@@ -3,7 +3,7 @@ package test;
 public class Fun1 {
 
 	public static void main(String[] args) {
-		System.out.println("this is develop");
+		System.out.println("this is master");
 	}
 	
 	public String getcount(){
